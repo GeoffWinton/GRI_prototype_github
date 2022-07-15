4e16604833797cd3efae23c5e16b83b37a8a7dad
GeoffWinton <66683586+GeoffWinton@users.noreply.github.com>
2022-07-15 11:55:13 +0100

    flip car functionality

---
+++ Source/GRI_PROTO/GRI_PROTO.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GRI_PROTO.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GRI_PROTO, "GRI_PROTO" );
